e4000403dbcdb538f647cf7fab24110f0c9992e1
ahmedbasemdev <ahmedbasem.dev@gmail.com>
2023-08-16 20:04:04 +0300

    b

--- featureExtraction/VideoPreprocessing.py
+++ featureExtraction/VideoPreprocessing.py
@@ -1,9 +1,9 @@
 from moviepy.editor import VideoFileClip
 import math
 import numpy as np
-from utils import common
+# from utils import common
 
-
+frame_size = 224
 def get_frames(my_segment, size=30):
     """
     :keyword
@@ -18,7 +18,7 @@ def get_frames(my_segment, size=30):
     for my_frame in my_segment.iter_frames():
         frames.append(my_frame)
     # print(np.array(frames).shape)
-    frames = np.array(frames).reshape((3, common['FRAME_SIZE'], common['FRAME_SIZE'], -1))
+    frames = np.array(frames).reshape((3, frame_size, frame_size, -1))
     try:
         random_indices = np.random.choice(frames.shape[0], size=size, replace=False)
         frames = frames[random_indices]
@@ -39,10 +39,10 @@ def split_video(video_path, num_segments, num_frames):
     :return: segments array with shape ( num_segments , num_frames , width , height)
     """
 
-    bagof_segments = np.zeros((num_segments, 3, num_frames, common['FRAME_SIZE'], common['FRAME_SIZE']))
+    bagof_segments = np.zeros((num_segments, 3, num_frames, frame_size, frame_size))
     # Load the video clip
     clip = VideoFileClip(video_path)
-    clip = clip.resize((common['FRAME_SIZE'], common['FRAME_SIZE']))
+    clip = clip.resize((frame_size, frame_size))
 
     # Get the duration of the video in seconds
     duration = clip.duration
